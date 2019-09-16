b1951b74ffcd80a2cbdafefd3501f84babe25d71
bchutchison <benhutchison85@gmail.com>
2019-09-16 13:36:13 +0100

    initial

---
+++ src/main/java/com/codeclan/employeetrackerlab/employeetracker/models/Employee.java
@@ -0,0 +1,48 @@
+package com.codeclan.employeetrackerlab.employeetracker.models;
+
+public class Employee {
+
+    private String name;
+    private int age;
+    private int employeeNumber;
+    private String email;
+
+    public Employee(String name, int age, int employeeNumber, String email) {
+        this.name = name;
+        this.age = age;
+        this.employeeNumber = employeeNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
